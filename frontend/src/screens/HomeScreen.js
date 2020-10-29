6a4c7a100f9f731101c92530b4c26ebf23b0ca2e
Theelipan prabakar <theelipan@gmail.com>
2020-10-29 16:13:33 -0400

    show product image, rating  and price

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import products from '../products';
 import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
 
 const HomeScreen = () => {
   return (
@@ -8,8 +9,8 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4}>
-            <h2>{product.name}</h2>
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} key={product._id}></Product>
           </Col>
         ))}
       </Row>
