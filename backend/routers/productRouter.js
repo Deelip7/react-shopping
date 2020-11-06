8ec44e776384513717b680daa4849b3e1b4abe93
Theelipan prabakar <theelipan@gmail.com>
2020-11-05 19:25:28 -0500

    add product router

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Product from '../models/productModel';
+import Product from '../models/productModel.js';
 import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
@@ -15,18 +15,14 @@ router.get(
   })
 );
 
-// @desc Fetch single product
-// @route Get api/products/:id
-// @access  Public
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: 'Product Not Founds' });
+      res.status(404).json({ message: 'Product not found' });
     }
   })
 );
