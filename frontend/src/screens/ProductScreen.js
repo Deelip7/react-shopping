0e2e5a6b987fba45a7a95d8899c8f35290641561
Theelipan prabakar <theelipan@gmail.com>
2020-11-09 13:54:33 -0500

    add to cart and get save cart info in localStorage

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,7 +8,7 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
 
   const productDetails = useSelector((state) => state.productDetails);
@@ -19,7 +19,7 @@ const ProductScreen = ({ history, match }) => {
   }, [dispatch, match]);
 
   const addToCartHandler = () => {
-    history.push(`./cart/${match.params.id}?qty=${qty}`);
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
   };
 
   return (
@@ -82,7 +82,7 @@ const ProductScreen = ({ history, match }) => {
                 )}
 
                 <ListGroup.Item>
-                  <Button onClick={addToCartHandler} className='btn-block ' type='Button' disabled={product.countInStock === 0}>
+                  <Button onClick={addToCartHandler} className='btn-block' type='Button' disabled={product.countInStock === 0}>
                     ADD TO CART
                   </Button>
                 </ListGroup.Item>
