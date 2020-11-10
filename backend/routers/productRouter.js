820119a84805c0103478ffc112c35676365311b7
Theelipan prabakar <theelipan@gmail.com>
2020-11-09 19:22:29 -0500

    add product controller and clean up router

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -1,32 +1,9 @@
 import express from 'express';
-import Product from '../models/productModel.js';
-import asyncHandler from 'express-async-handler';
+import { getProducts, getProductById } from '../controllers/productController.js';
 
 const router = express.Router();
 
-// @desc Fetch all products
-// @route Get api/products
-// @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // res.status(404);
-    // throw new Error('THIS IS AN ERROR MSG');
-    res.json(products);
-  })
-);
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: 'Product not found' });
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
