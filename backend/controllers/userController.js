eedb167360d4addc8640313ab2aba1413d3058e1
Theelipan prabakar <theelipan@gmail.com>
2020-11-09 21:45:11 -0500

    add user auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,22 +31,19 @@ const authUser = asyncHandler(async (req, res) => {
 // @access  Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('asdasdasdasd');
-  //   const user = await User.findById(req.user._id);
-  //   console.log('user._id', user._id);
+  const user = await User.findById(req.user._id);
 
-  //   if (user && (await user.matchPassword(password))) {
-  //     res.json({
-  //       _id: user._id,
-  //       name: user.name,
-  //       email: user.email,
-  //       isAdmin: user.isAdmin,
-  //       token: generateToken(user._id),
-  //     });
-  //   } else {
-  //     res.status(401);
-  //     throw new Error('Invaild email or password');
-  //   }
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('User not found -Invaild email or password');
+  }
 });
 
 export { authUser, getUserProfile };
--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/profile', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
